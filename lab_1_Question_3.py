96cbc0620f5af8c672bf404c2b8eece59be12fc8
Muhammad Furqan Mohsin <furqanmohammed788@gmail.com>
2024-09-20 00:36:23 +0500

    Add files via upload

---
+++ lab_1_Question_3.py
@@ -0,0 +1,6 @@
+length_of_rectangle = float(input("Enter the value of length: "))
+width_of_rectangle = float(input("Enter the value of width: "))
+
+Area_of_Rectangle = length_of_rectangle * width_of_rectangle
+
+print("The area of the rectangle is", Area_of_Rectangle)
