7b5f12e951ffc602206966d445f025e22f88bc70
Muhammad Furqan Mohsin <furqanmohammed788@gmail.com>
2024-09-20 19:49:34 +0500

    Add files via upload

---
+++ lab_2_Question_1.py
@@ -0,0 +1,37 @@
+#(a) The sum of the first five positive integers.
+
+sum_of_integers=1+2+3+4+5
+print("the sum of first five positive integers is: ",sum_of_integers)
+
+#(b) The average age of Sara (age 23), Mark (age 19), and Fatima (age 31).
+
+sara_age=23
+mark_age=19
+fatima_age=31
+avg_age=(sara_age+mark_age+fatima_age)/3
+print("the average age is: ",round(avg_age,2))
+
+#(c) The number of times 73 goes into 403.
+
+times_73_goes_into_403=403//73
+print("the number of times 73 goes into 403 is: ",times_73_goes_into_403)
+
+#(d) The remainder when 403 is divided by 73.
+
+remainder_is=403%73
+print("The remainder when 403 is divided by 73",remainder_is)
+
+#(e) 2 to the 10th power.
+
+power_of_2=2**10
+print("2 to the 10th power is: ",power_of_2)
+
+#(f) The absolute value of the difference between Sara’s height (54 inches) and Mark’s height (57 inches).
+
+absolute_value=abs(54-57)
+print("the absolute value of the difference of height is: ")
+
+#(g) The lowest price among the following prices: $34.99, $29.95, and $31.50.
+
+lowest_price=min(34,99,29,95,31,50)
+print("the lowest price among the following prices is: ",lowest_price)
