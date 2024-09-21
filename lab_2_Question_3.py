99d4f4204898ea22c95d5b166bef04851bfbee08
Muhammad Furqan Mohsin <furqanmohammed788@gmail.com>
2024-09-21 12:18:47 +0500

    Add files via upload

---
+++ lab_2_Question_3.py
@@ -0,0 +1,7 @@
+P = int(input("Enter the Principle amount in Rs: "))
+R = float(input("Enter the Rate in percentage: "))
+T = int(input("Enter the Time in years: "))
+
+
+simple_interest = (P * R * T) / 100
+print("Simple interest in Rs is:", simple_interest)
