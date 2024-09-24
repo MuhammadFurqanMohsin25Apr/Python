ccc8c936e4a95de5f282cb5180b6e598f02df764
Muhammad Furqan Mohsin <furqanmohammed788@gmail.com>
2024-09-24 12:16:26 -0700

    lab_3_Question_1.py

---
+++ lab_3_Question_1.py
@@ -0,0 +1,12 @@
+# KNOWN:
+# Radius (r) in meters:
+r = float(input("Enter the radius of a circle: "))
+
+# Angular Speed (ω) in radians/second:
+ω = float(input("Enter the Angular Speed: "))
+
+# We need to find linear velocity:
+# FORMULA:
+v = r * ω
+
+print("The linear velocity of a ball is:", v, "meters/second")
