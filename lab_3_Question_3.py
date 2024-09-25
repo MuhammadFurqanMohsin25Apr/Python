a49931f24bd7b3520030bcaebbd56adc63864172
Muhammad Furqan Mohsin <furqanmohammed788@gmail.com>
2024-09-25 11:29:30 +0500

    Add files via upload

---
+++ lab_3_Question_3.py
@@ -0,0 +1,13 @@
+# KNOWN
+# Radius (r) in meters:
+r = float(input("Enter the radius:"))
+
+# Linear velocity (v) in meters/second:
+v = float(input("Enter the linear velocity:"))
+
+# We need to find angular velocity:
+
+# FORMULA:
+ω = v / r
+
+print("The magnitude of the angular velocity is:", ω, "radians/second")
