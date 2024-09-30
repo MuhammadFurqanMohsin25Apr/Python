f368e813a2d1d0563110bf4dab4d639911fbd883
Muhammad Furqan Mohsin <furqanmohammed788@gmail.com>
2024-09-30 12:49:51 -0700

    lab_4_Question_3.py

--- lab_4_Question_3.py
+++ lab_4_Question_3.py
@@ -1,6 +1,6 @@
 p = input("Enter the string: ")
-p = p.casefold()  # Convert the string to lowercase to handle case-insensitivity
-e = "".join(reversed(p))  # Reverse the string
+p = p.casefold()  
+e = "".join(reversed(p))  
 
 if p == e:
     print("Yes, your string is a palindrome")
