78ef299a7506bc431c9202c553930a5f6a0f3b80
Muhammad Furqan Mohsin <furqanmohammed788@gmail.com>
2024-10-12 18:20:09 +0500

    Add files via upload

---
+++ lab_6_Question_5.py
@@ -0,0 +1,17 @@
+# (a):
+a = 6
+b = 7
+
+# (b):
+c = (a + b) / 2
+
+# (c):
+inventory = ['paper', 'staples', 'pencils']
+
+# (d):
+first = 'John'
+middle = 'Fitzgerald'
+last = 'Kennedy'
+
+# (e):
+fullname = first + " " + middle + " " + last
