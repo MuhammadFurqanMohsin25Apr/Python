d5e29c5dac844eaebd8de1c4c022fd20d3536b96
Muhammad Furqan Mohsin <furqanmohammed788@gmail.com>
2024-10-26 06:48:21 -0700

    Add files via upload

---
+++ lab_7_Question_3.py
@@ -0,0 +1,15 @@
+# (a)
+a = len("anachronistically") - len("counterintuitive") == 1
+print("The number of characters in 'counterintuitive' is one less than the characters in 'anachronistically' is", a)
+
+# (b)
+b = "misinterpretation" < "misrepresentation"
+print("The word 'misinterpretation' appears earlier in the dictionary than 'misrepresentation' is", b)
+
+# (c)
+not_in = "e" not in "floccinaucinihilipilification"
+print("'e' is not in 'floccinaucinihilipilification' is", not_in)
+
+# (d)
+d = len("counterrevolution") == (len("counter") + len("resolution"))
+print("The number of characters in 'counterrevolution' is equal to the sum of characters of 'counter' and 'resolution' is", d)
